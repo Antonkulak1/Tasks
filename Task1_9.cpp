738d73b9bd554c1d5a55c6471be6b20b83e6cf3d
Antonkulak1 <kul4ckov.antony@yandex.ru>
2025-07-02 22:27:42 +0300

    Add files via upload

---
+++ Task1_9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/*Вывести на экран числа 50 и 10 одно под другим.*/
+int main()
+{
+    {
+        std::cout << 50 << std::endl;
+        std::cout << 10 << std::endl;
+    }
+    return 0;
+}
